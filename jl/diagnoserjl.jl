47473696d45a9595a7f3dd7904f64d2a86c8af40
Robert Schnitman <robertschnitman@gmail.com>
2018-03-16 14:33:41 -0500

    Don't import Gadfly until diagnose() is developed

--- jl/diagnoserjl.jl
+++ jl/diagnoserjl.jl
@@ -2,9 +2,10 @@ __precompile__()
 
 module diagnoserjl
 	using DataFrames
-	using Gadfly
+	# using Gadfly # don't import Gadfly until diagnose() has been developed.
 	using GLM
 	
 	include("fitres.jl")
+	include("modeldf.jl")
 	
 end # module
