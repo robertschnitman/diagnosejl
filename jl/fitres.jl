1d1f03d620f509d91885d316ee38f137e8aafa89
Robert Schnitman <robertschnitman@gmail.com>
2018-03-25 15:58:08 -0500

    Include docstrings - help documentation

--- jl/fitres.jl
+++ jl/fitres.jl
@@ -1,11 +1,32 @@
+"""
+    fitres(model, nothing)
+	
+Generate fitted values and residuals into one array (or merge with a DataFrame).
+
+INPUT: GLM object.
+
+OUTPUT: array (or DataFrame if dataframe is set to a DataFrame object)).
+
+# Definitions
+fit = Fitted Values based on model object.  
+
+residual = Model residuals.  
+
+residual_margin = Model residuals in percentage (proportion) form.
+
+# Examples
+See https://github.com/robertschnitman/diagnoserjl.
+
+"""
+
 #####################################################################################
 ### Robert Schnitman
 ### 2018-03-12
 ###
-### PURPOSE: Generate fitted values and residuals into one array (or DataFrame).
+### PURPOSE: Generate fitted values and residuals into one array (or merge with a DataFrame).
 ###
-### INPUT: GLM object.
-### OUTPUT: array (or DataFrame).
+### INPUT: GLM object.  
+### OUTPUT: array (or DataFrame if dataframe is set to a DataFrame object).
 ###
 ### RECOMMENDED CITATION:
 ###  Schnitman, Robert (2018). fitres.jl. https://github.com/robertschnitman/diagnoserjl
@@ -29,10 +50,10 @@ function fitres(model, data = nothing)
   if (data == nothing)  
   
     fitresm # Array was the original plan a la` the matrix output in its R equivalent, 
-	          #   but unnamed column headers in base Julia was a problem (i.e. which 
-			      #   column belongs to fitted values, etc?).
-            # NamedArrays.jl could be an option, but DataFrames has more user reach.
-			      # Users can always use convert() to obtain an Array instead.
+	        #   but unnamed column headers in base Julia was a problem (i.e. which 
+			#   column belongs to fitted values, etc?).
+	        # NamedArrays.jl could be an option, but DataFrames has more user reach.
+			# Users can always use convert() to obtain an Array instead.
 	
   elseif (datatype == "DataFrames.DataFrame")    
 	
