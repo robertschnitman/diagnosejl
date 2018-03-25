26517da061625ecc61b30e89ce6263e33b53e36e
Robert Schnitman <robertschnitman@gmail.com>
2018-03-25 18:39:18 -0500

    Update diagnoserjl.jl

--- src/diagnoserjl.jl
+++ src/diagnoserjl.jl
@@ -1,13 +1,14 @@
 __precompile__()
 
 module diagnoserjl
-	using DataFrames
-	using Gadfly
-	using GLM
+
+using DataFrames
+using Gadfly
+using GLM
 	
-	include("diagnose.jl")
-	include("fitres.jl")
-	include("modeldf.jl")
-        include("validate.jl")
+include("diagnose.jl")
+include("fitres.jl")
+include("modeldf.jl")
+include("validate.jl")
 	
 end # module
