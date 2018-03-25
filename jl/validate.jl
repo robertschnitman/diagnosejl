cb4fe628f28a526434501140da91da59cb5b0759
Robert Schnitman <robertschnitman@gmail.com>
2018-03-25 15:26:17 -0500

    New statistics.
    1. Median Percentage Error can be used when the distribution is skewed.
    
    2. Standard Deviation Percentage Error indicates the average distance from the Mean Percentage Error.

--- jl/validate.jl
+++ jl/validate.jl
@@ -2,7 +2,7 @@
 ### Robert Schnitman
 ### 2018-03-16
 ###
-### PURPOSE: Common model statistics such as F, R^2, and RMSE in a vector.
+### PURPOSE: Produce common model statistics such as F, R^2, and RMSE in a vector.
 ###
 ### RECOMMENDED CITATION:
 ###  Schnitman, Robert (2018). validate.jl. https://github.com/robertschnitman/diagnoserjl
@@ -11,27 +11,35 @@
 ##### === BEGIN === #####
 function validate(model, dataframe = false)
 
-  ### Type-checking ###
+  ### 1. Type-checking ###
   modeltype = string(typeof(model))  
   contains(modeltype, "GLM") || error("This function only accepts GLM classes for now. Sorry!")  
 
-  ### Statistics ###
+  ### 2. Statistics ###
   # deviance_residual = deviance(model) # Need only on GLM models, not OLS.
-  mae             = mean(abs.(residuals(model)))
-  mpe             = mean(residuals(model)./model.mf.df[1])  
+  
+  r      = residuals(model) # Easier to read how the final statistics are computed.
+  depvar = model.mf.df[1]   # Same as above.
+  
+  
+  mae             = mean(abs.(r))
+  medianpe        = median(r./depvar)
+  mpe             = mean(r./depvar)
+  sdpe            = std(r./depvar)
   n               = nobs(model)
-  residual_median = median(residuals(model))
-  residual_mean   = mean(residuals(model))
-  residual_sd     = std(residuals(model))
-  rmse            = sqrt(mean(residuals(model).^2))
+  residual_median = median(r)
+  residual_mean   = mean(r)
+  residual_sd     = std(r)
+  rmse            = sqrt(mean(r.^2))
   rsq             = r2(model)
   ar2             = adjr2(model)
   # Need F-test and its p-value.
   
-  ### Print Output
-  common = DataFrame(n = n, ar2 = ar2, r2 = rsq, rmse = rmse, mae = mae, mpe = mpe, residual_mean = residual_mean, residual_median = residual_median, residual_sd = residual_sd)
+  ### 3. Print Output ###  
   
-  pivotdf = DataFrame(statistics = names(common), value = round.(common[1, :].columns, 6))
+  common = DataFrame(n = n, ar2 = ar2, r2 = rsq, rmse = rmse, mae = mae, medianpe = medianpe, mpe = mpe, sdpe = sdpe, residual_mean = residual_mean, residual_median = residual_median, residual_sd = residual_sd)
+  
+  pivotdf = DataFrame(statistic = names(common), value = round.(common[1, :].columns, 6))
   
   if (dataframe == false)
     convert(Array, pivotdf)
