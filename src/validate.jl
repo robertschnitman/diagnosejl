d70cb05e6364f23a5226c35468c445e016fdf9d9
Robert Schnitman <robertschnitman@gmail.com>
2018-03-27 00:04:57 -0500

    New statistic: Apparent Error Rate.
    Can now produce statistics for non-OLS cases.

--- src/validate.jl
+++ src/validate.jl
@@ -5,13 +5,15 @@
 Produce common model statistics such as R^2, RMSE, and MPE in a vector. The model input is a GLM object, outputting to an array if dataframe = false.
 
 # Definitions
+  aer             = Apparent Error Rate, defined as the proportion of misclassifications relative to total number of cases.
+
   ar2             = Adjusted R-Squared.  
   
   mad             = Median Absolute Deviation.  
   
   mae             = Mean Absolute Error.  
   
-  medianpe        = Median Absolute Error.
+  medianpe        = Median Percent Error.
   
   mpe             = Mean Percentage Error.  
   
@@ -51,34 +53,50 @@ function validate(model, dataframe = false)
   modeltype = string(typeof(model))  
   contains(modeltype, "GLM") || error("This function only accepts GLM classes for now. Sorry!")  
 
-  ### 2. Statistics ###
-  # deviance_residual = deviance(model) # Need only on GLM models, not OLS.
-  
-  r      = residuals(model) # Easier to read how the final statistics are computed.
-  depvar = model.mf.df[1]   # Same as above.
-  
-  mad             = median(abs.(r .- median(r)))
-  mae             = mean(abs.(r))
-  medianpe        = median(r./depvar)
-  mpe             = mean(r./depvar)
-  sdpe            = std(r./depvar)
+  ### 2. Common Statistics ###
+  #  # Need only on GLM models, not OLS.
   n               = nobs(model)
-  residual_median = median(r)
-  residual_mean   = mean(r)
-  residual_sd     = std(r)
-  rmse            = sqrt(mean(r.^2))
-  rsq             = r2(model)
-  ar2             = adjr2(model)
+  depvar = model.mf.df[1]   # Same as above.
+  yhat   = predict(model)
+
   # Need F-test and its p-value.
+
   
-  ### 3. Print Output ###  
+  ### 3. Print Output ###      
   
-  common = DataFrame(n = n, ar2 = ar2, r2 = rsq, rmse = rmse, mad = mad, mae = mae, medianpe = medianpe, mpe = mpe, sdpe = sdpe, residual_mean = residual_mean, residual_median = residual_median, residual_sd = residual_sd)
+  if contains(modeltype, "GLM.LinearModel")
+    r      = residuals(model) # Easier to read how the final statistics are computed.
   
-  pivotdf = DataFrame(statistic = names(common), value = round.(common[1, :].columns, 6))
+	mad             = median(abs.(r .- median(r)))
+	mae             = mean(abs.(r))
+	medianpe        = median(r./depvar)
+	mpe             = mean(r./depvar)
+	sdpe            = std(r./depvar)
+	
+	residual_median = median(r)
+	residual_mean   = mean(r)
+	residual_sd     = std(r)
+	rmse            = sqrt(mean(r.^2))
+	rsq             = r2(model)
+	ar2             = adjr2(model)
+	
+	output = DataFrame(n = n, ar2 = ar2, r2 = rsq, rmse = rmse, mad = mad, mae = mae, medianpe = medianpe, mpe = mpe, sdpe = sdpe, residual_mean = residual_mean, residual_median = residual_median, residual_sd = residual_sd)    
+    
+  elseif contains(modeltype, "Binomial")
+    aer = function(y, yhat)
+	  length(yhat[yhat != y])/length(y)
+    end
+	
+    aer = length(yhat[yhat != depvar])/length(depvar)
+	
+	output = DataFrame(n = n, deviance_residual = deviance(model), aer = aer) 
+  
+  end
+  
+  pivotdf = DataFrame(statistic = names(output), value = round.(output[1, :].columns, 6))
   
   if (dataframe == false)
-    convert(Array, pivotdf)
+    convert(Array, pivotdf) # statistic column otputs keys
   else
     pivotdf
   end
