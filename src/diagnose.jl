3b26a2e40010378dc0b44cfe4e15ab1f446f5c94
Robert Schnitman <robertschnitman@gmail.com>
2018-03-27 20:56:52 -0500

    Labeling issues
    May also want to retire "residuals margin" for the simpler "residuals (%)"...

--- src/diagnose.jl
+++ src/diagnose.jl
@@ -1,7 +1,7 @@
 """
     diagnose(model)
 	
-Scatterplots of fit vs. residuals/residuals margin, as well as histograms of residuals/residuals margin
+Scatterplots of fit vs. residuals/residuals , as well as histograms of residuals and residuals %.
 
 INPUT: GLM object.
 
@@ -17,7 +17,7 @@ See https://github.com/robertschnitman/diagnoserjl.
 ### Robert Schnitman
 ### 2018-03-21
 ###
-### PURPOSE: Scatterplots of fit vs. residuals/residuals margin, as well as histograms of residuals/residuals margin.
+### PURPOSE: Scatterplots of fit vs. residuals/residuals , as well as histograms of residuals/residuals .
 ###
 ### INPUT: GLM object.
 ### OUTPUT: 2x2 graph.
@@ -38,18 +38,17 @@ function diagnose(model)
   fit = predict(model)      # Fitted values.
   res = residuals(model)    # Residuals.
   act = res + fit           # Actual values. 
-  rem = res./act            # Residuals Margin
+  rem = (res./act).*100     # Residuals %.
   
   
   ### 3. Print Output ###
   rvf1 = scatter(fit, res, ylabel = "Residuals", title = "Residuals vs. Fitted Values")
 				
-  rvf2 = scatter(fit, rem, ylabel = "Residuals Margin (%)", title = "Residuals (%) vs. Fitted Values")
+  rvf2 = scatter(fit, rem, ylabel = "Residuals (%)", title = "Residuals (%) vs. Fitted Values")
 
   hist1 = histogram(res, xlabel = "Residuals", ylabel = "Frequency", bins = length(fit))
-  hist2 = histogram(rem, xlabel = "Residuals Margin (%)", ylabel = "Frequency", bins = length(fit))
+  hist2 = histogram(rem, xlabel = "Residuals (%)", ylabel = "", bins = length(fit))
   
-  # gridstack([rvf1 rvf2; hist1 hist2])
   plot(rvf1, rvf2, hist1, hist2, layout = (2,2), legend = false)
   
 end
